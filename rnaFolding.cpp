9827c28bff95c0c14ef09bd4882d2575d042f3b7
Nikhil Dhanaraj <f20210427@hyderabad.bits-pilani.ac.in>
2024-04-27 16:06:33 +0530

    add comments

--- rnaFolding.cpp
+++ rnaFolding.cpp
@@ -16,12 +16,12 @@ bool pairMatch(char base1, char base2){
            (base1 == 'G' && base2 == 'C');
 }
 
-vector<vector<int> > findMaxPairs(const string& rna_sequence) {
-    int n = rna_sequence.length();
-    int edge = n - 5;
-    vector<vector<int>> dp(n, vector<int>(n, 0));
+vector<vector<int> > findMaxPairs(const string& rna_sequence){   ///function that returns the maximum number of base pairs in the rna sequence
+    int n = rna_sequence.length();   ///calculating the length of the rna sequence
+    int edge = n - 5;  ///sequence length of the base pairs must be atleast 5 hence edge initilaized to n-5
+    vector<vector<int>> dp(n, vector<int>(n, 0)); ///dp table to calculate optimum sequence for rna secondary structure prediction
 
-    for(int i = 0; i < edge; i++){
+    for(int i = 0; i < edge; i++){  ///filling in dp table that calculates the maximum number of pairs that can be formed by taking or not taking the current pair
         int row = i;
         int col = n - 1;
         while(row < edge && col >= 5){
@@ -44,12 +44,12 @@ vector<vector<int> > findMaxPairs(const string& rna_sequence) {
     return dp;
 }
 
-vector<vector<int> > findSplits(const string& rna_sequence, vector<vector<int> > & numPairs) {
-    int n = rna_sequence.length();
-    int edge = n - 5;
-    vector<vector<int> > dp(n, vector<int>(n, -1));
-    int splitIndex = 0;
-    for(int i = 0; i < edge; i++){
+vector<vector<int> > findSplits(const string& rna_sequence, vector<vector<int> > & numPairs){ ///function that finds the optimal index at which a sequence is split into subproblems
+    int n = rna_sequence.length();   ///calculating the length of the rna sequence
+    int edge = n - 5;  ///sequence length of the base pairs must be atleast 5 hence edge initilaized to n-5
+    vector<vector<int> > dp(n, vector<int>(n, -1));  ///dp table to calculate optimum sequence for rna secondary structure prediction
+    int splitIndex = 0;  ///variable to store where a split occurs in the rna molecule initialized to 0
+    for(int i = 0; i < edge; i++){  ///iterates over the elements of the dp table
         int row = i;
         int col = n - 1;
         while(row < edge && col >= 5){
@@ -66,7 +66,7 @@ vector<vector<int> > findSplits(const string& rna_sequence, vector<vector<int> >
                     splitIndex = t;
                 }
             }
-            dp[row][col] = (taken > notTaken) ? splitIndex : dp[row][col-1];
+            dp[row][col] = (taken > notTaken) ? splitIndex : dp[row][col-1];  ///filling in the dp table
             row++;
             col--;
 
@@ -75,7 +75,7 @@ vector<vector<int> > findSplits(const string& rna_sequence, vector<vector<int> >
     return dp;
 }
 
-void printMatrix(vector<vector<int> > dp){
+void printMatrix(vector<vector<int> > dp){   ///prints the 2d table
     int n = dp.size();
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
@@ -85,16 +85,16 @@ void printMatrix(vector<vector<int> > dp){
     }
 }
 
-vector<pair<int, int> > matchingPairs (const string& rna_sequence, vector<vector<int> > &splitIndices){
-    int n = rna_sequence.length();
-    int edge = n - 5;
+vector<pair<int, int> > matchingPairs (const string& rna_sequence, vector<vector<int> > &splitIndices){   ///function that returns the index of the pairs in the sequence
+    int n = rna_sequence.length();  ///calculating the length of the rna sequence
+    int edge = n - 5;   /// sequence length of the base pairs must be atleast 5 hence edge initilaized to n-5
     queue<pair<int,int> > q;
     vector<pair<int,int> > matchingIndices;
-    int leftInd = edge - 1;
-    int rightInd = n - 1;
-    q.push(std::make_pair(leftInd, rightInd));
-    while(!q.empty()){
-        leftInd = q.front().first;
+    int leftInd = edge - 1;  ///setting leftIndex to n-6th index
+    int rightInd = n - 1;   ///setting leftIndex to n-1th index
+    q.push(std::make_pair(leftInd, rightInd));  ///setting leftIndex and rightIndex as the first pair
+    while(!q.empty()){  ///does bfs on the queue that finds the split for each half until there are no splits left
+        leftInd = q.front().first;  
         rightInd = q.front().second;
         q.pop();
         if(leftInd < 0 || leftInd > n - 1 || rightInd < 0 || rightInd > n - 1) continue;
@@ -105,10 +105,10 @@ vector<pair<int, int> > matchingPairs (const string& rna_sequence, vector<vector
             q.push(std::make_pair(edge - 2 - split, rightInd - 1));
         }
     }
-    return matchingIndices;
+    return matchingIndices;   ///return index pairs of the sequence
 }
 
-void printOutputs(string rna_sequence,int rnaLen,string actualDotBracket,int numOptimalPairs,int brackCount,string dotBracket, vector<pair<int,int>> pairs){
+void printOutputs(string rna_sequence,int rnaLen,string actualDotBracket,int numOptimalPairs,int brackCount,string dotBracket, vector<pair<int,int>> pairs){   ///function used to print the final outputs of the program
     cout << line << endl;
     cout << "The given rna sequence is [Mus musculus SSU rRNA]: \n" << rna_sequence << endl;
     cout << line << endl;
@@ -127,52 +127,51 @@ void printOutputs(string rna_sequence,int rnaLen,string actualDotBracket,int num
 }
 
 string calculateOptimalSequence(string rna_sequence, vector<vector<int>> &numPairs, vector<vector<int>> &splitIndices, vector<pair<int,int>> &pairs){
-    numPairs = findMaxPairs(rna_sequence);
-    splitIndices = findSplits(rna_sequence, numPairs);
-    pairs = matchingPairs(rna_sequence, splitIndices);
+    numPairs = findMaxPairs(rna_sequence);  ///find the maximum number of base pairs in the rna sequence
+    splitIndices = findSplits(rna_sequence, numPairs);  ///finds the optimal index at which a sequence is split into subproblems
+    pairs = matchingPairs(rna_sequence, splitIndices);  ///returns the index of the pairs in the sequence
     
-    int rnaLen = rna_sequence.length();
+    int rnaLen = rna_sequence.length();  ///length of rna sequence
     string dotBracket;
-    for(int i = 0; i < rnaLen; i++){
+    for(int i = 0; i < rnaLen; i++){  ///initializes dotBracket with dots to represent unpaired bases
         dotBracket.push_back('.');
     }
-    for(const pair<int,int> pairInst : pairs){
+    for(const pair<int,int> pairInst : pairs){  ///this loop iterates through each pair in the base pair list and accordingly replaces the dot with '(' or ')'
         int leftBrack = pairInst.first;
         int rightBrack = pairInst.second;
         dotBracket[leftBrack] = '(';
         dotBracket[rightBrack] = ')';
     }
-    return dotBracket;
+    return dotBracket;  ///retruning the final dot bracket sequence
 }
 
-void predictSecondaryStructure(string rna_sequence,string actualDotBracket){
-    int rnaLen = rna_sequence.length();
-    if(rnaLen <= 5){
+void predictSecondaryStructure(string rna_sequence,string actualDotBracket){ /// function that predicts the secondary structure of the rna molecule
+    int rnaLen = rna_sequence.length();  /// calculating the length of the rna sequence
+    if(rnaLen <= 5){  ///if the length of the rna sequence is less than or equal to 5 then there are no matching pairs in that sequence
         cout << "There are 0 matching pairs. Please enter another sequence." << endl;
         return;
     }
-    int brackCount = 0;
+    int brackCount = 0;    ///index to count the total number of '(' brackets in the dot bracket sequence
     for(char c : actualDotBracket){
-        if(c == '(') brackCount++;
+        if(c == '(') brackCount++;   ///increasing the count of brackCount for every open bracket found in the dot bracket sequence
     }
     vector<vector<int>> numPairs, splitIndices;
     vector<pair<int,int>> pairs;
     
-    string dotBracket = calculateOptimalSequence(rna_sequence,numPairs,splitIndices,pairs);
+    string dotBracket = calculateOptimalSequence(rna_sequence,numPairs,splitIndices,pairs);  ///calling the function that calculates the optimal sequence for the secondary structure of the rna
 
-    printOutputs(rna_sequence, rnaLen, actualDotBracket, numPairs[rnaLen-6][rnaLen-1], brackCount, dotBracket, pairs);
-
+    printOutputs(rna_sequence, rnaLen, actualDotBracket, numPairs[rnaLen-6][rnaLen-1], brackCount, dotBracket, pairs);  ///printing the outputs of the program
 }
 
-void computeExecutionTime(string rna_sequence,string actualDotBracket){
-    
-    auto start = high_resolution_clock::now();
+void computeExecutionTime(string rna_sequence,string actualDotBracket){ ///function that computes the execution time that is taken to predict the secondary structure of the rna
 
-    predictSecondaryStructure(rna_sequence,actualDotBracket);
+    auto start = high_resolution_clock::now();  ///starting the clock
 
-    auto stop = high_resolution_clock::now();
+    predictSecondaryStructure(rna_sequence,actualDotBracket);  ///calling the function that predicts the secondary structure of the rna molecule
 
-    auto duration = duration_cast<milliseconds>(stop - start);
+    auto stop = high_resolution_clock::now();  ///stopping the clock
+
+    auto duration = duration_cast<milliseconds>(stop - start);  ///calculating the execution time of the function that predicts the secondary structure of the rna molecule
 
     cout << "\nThe time taken for the program to execute is: " << duration.count() << "ms" << endl;
 }
@@ -181,7 +180,7 @@ int main(){
     string actualDotBracket = "...((((.........))))(((.((((((...(.((..((.....(((.(((..((....((....((..........))...)).))......(((.......(((.(.(..((....(((....................((.....((.(((.....))).))......)).........((((...((((((......))))))...))))((..(((((..............(((.((.(((((((((((((....))))))))).))))...)))))....(......)..)))))......)).(.((((.((((......)))))))))...)))...))).).))).(((....(((....(((((((.........))))))))))......)))...(((.((((....))))....)))))).((.(((..........))).)).(.((....)).)...)))))).).....(.(((....(((.....)))..)))).)...)).).....(((.(((((.(((....))).).))))..)))......((..(...........)..)).........(((((........((((.....((....)).......)))).)))))..)))))).))).........(.(.......(.((...(((.((.....((.(.((((((((((((((((....)))).)..))))))))))).).))....((.(..(((.(((((.(.(((((((......)))))))..).))))))))).))..((((((.(.......((..((.......))((((.......))))...))......).)))..))).......((.(..(.((.((...............)).)).)..)))....))...(((((((..(...((((..(((.((((((((...((........))......)))))))).))).......((....))...))))..)..))).)))..))))...)).)....((((((....(...((((.........))))...).))))))..........(((.((.(((..(.((((((.(((((....))))))))))).)..)))...((....))...)).....))).).).(((......";
     
     
-    computeExecutionTime(rna_sequence,actualDotBracket);
+    computeExecutionTime(rna_sequence,actualDotBracket); ///calling the function that computes the execution time that is taken to predict the secondary structure of the rna
 
     
     return 0;
